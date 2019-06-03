86e313054bd7385cda63546a4e1de2a3844cdbe9
raissamartinsmenezes <raissamartinsdemenezes@gmail.com>
2019-06-03 16:49:32 -0300

    primeira aula de javascript 04

---
+++ javascript04/orientacaoobjeto.js
@@ -0,0 +1,22 @@
+//um conjunto de vários objetos
+class Botao {
+    constructor(cor, tamanho, texto) {
+        this.cor = cor
+        this.tamanho = tamanho
+        this.texto = texto
+    }
+
+    desenhaBotao() {
+        const novoBotaoNoDom = document.createElement('button')
+        novoBotaoNoDom.setAttribute('class', `${this.cor} ${this.tamanho}`)
+        novoBotaoNoDom.innerHTML = this.texto
+        document.getElementById('buttons-section').appendChild(novoBotaoNoDom)
+        console.log(novoBotaoNoDom)
+    }
+}
+
+const novoBotao = new Botao('verde', 'grande', 'click me!');
+novoBotao.desenhaBotao()
+
+const botaoRoxo = new Botao('roxo', 'pequeno', 'click me!');
+botaoRoxo.desenhaBotao()
